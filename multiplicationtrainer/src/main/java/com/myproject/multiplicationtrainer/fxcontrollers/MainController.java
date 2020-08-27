f066e9966f9dd0b8c3785b89e8bbf81f82c60337
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-27 15:44:33 +0300

    add menu bar

--- multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/fxcontrollers/MainController.java
+++ multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/fxcontrollers/MainController.java
@@ -1,5 +1,11 @@
 package com.myproject.multiplicationtrainer.fxcontrollers;
 
+import javafx.fxml.FXML;
+
 public class MainController {
   
+  @FXML
+  private void exitAction() {
+    System.exit(0);
+  }
 }
