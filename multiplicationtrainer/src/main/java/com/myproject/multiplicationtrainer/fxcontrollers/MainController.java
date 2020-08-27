db2317aff2e247b2a2c1945b56dd567fc7ae7902
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-27 17:39:34 +0300

    to be continue)

--- multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/fxcontrollers/MainController.java
+++ multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/fxcontrollers/MainController.java
@@ -1,11 +1,26 @@
 package com.myproject.multiplicationtrainer.fxcontrollers;
 
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class MainController implements Initializable {
+  @FXML
+  private Label lblScoreRight;
+  @FXML
+  private Label lblScoreWrong;
   
   @FXML
   private void exitAction() {
     System.exit(0);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    lblScoreRight.setText("Right");
+    lblScoreWrong.setText("Wrong");
   }
 }
