0de847271d7cd11eaa1806fc71e7075e9dbb7bf6
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-27 18:55:17 +0300

    add style

--- multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/fxcontrollers/MainController.java
+++ multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/fxcontrollers/MainController.java
@@ -6,12 +6,17 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController implements Initializable {
   @FXML
   private Label lblScoreRight;
   @FXML
   private Label lblScoreWrong;
+  @FXML
+  private TextField tfScoreRight;
+  @FXML
+  private TextField tfScoreWrong;
   
   @FXML
   private void exitAction() {
@@ -22,5 +27,9 @@ public class MainController implements Initializable {
   public void initialize(URL location, ResourceBundle resources) {
     lblScoreRight.setText("Right");
     lblScoreWrong.setText("Wrong");
+    tfScoreRight.setText("0");
+    tfScoreRight.setEditable(false);
+    tfScoreWrong.setText("0");
+    tfScoreWrong.setEditable(false);
   }
 }
