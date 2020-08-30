06ed40d618b7c0513adcd01cb494534cf787c376
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-30 20:38:02 +0300

    refactorro)

--- multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/App.java
+++ multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/App.java
@@ -12,15 +12,20 @@ import com.myproject.multiplicationtrainer.core.TaskManager;
 import com.myproject.multiplicationtrainer.fxcontrollers.MainController;
 
 public class App extends Application {
+  private FXMLLoader loader;
+  private TaskManager taskManager;
+  private MainController mainController;
+  private Parent root;
+  private Scene scene;
   
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
-    TaskManager taskManager = new TaskManager();
-    MainController mainController = new MainController(taskManager);
+    loader = new FXMLLoader(getClass().getResource("main.fxml"));
+    taskManager = new TaskManager();
+    mainController = new MainController(taskManager);
     loader.setController(mainController);
-    Parent root = loader.load();
-    Scene scene = new Scene(root);
+    root = loader.load();
+    scene = new Scene(root);
     scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
     stage.setScene(scene);
     stage.setTitle("Rise your skills...");
