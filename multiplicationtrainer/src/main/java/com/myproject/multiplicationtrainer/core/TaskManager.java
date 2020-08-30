1f1898fd12caa78a2416238aac769bcb1d4d1e2b
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-30 20:35:55 +0300

    fill TaskManager class

--- multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/core/TaskManager.java
+++ multiplicationtrainer/src/main/java/com/myproject/multiplicationtrainer/core/TaskManager.java
@@ -1,17 +1,47 @@
 package com.myproject.multiplicationtrainer.core;
 
+import java.util.Random;
+
 import com.myproject.multiplicationtrainer.interfaces.GUIListener;
 
 public class TaskManager implements GUIListener {
+  
+  private final char multiOperator = 'x';
+  private final char divOperator = ':';
+  
+  private int a;
+  private int b;
+  private int c;
+  private int answer;
+  private Random random;
+  private boolean multiply;
+  
+  public TaskManager() {
+    random = new Random();
+    multiply = true;
+    init(9);
+  }
 
   @Override
   public String getTask() {
-    return "22 x 22";
+    if (multiply) {
+      answer = c;
+      multiply = false;
+      return String.format("%d %c %d", a, multiOperator, b);
+    }
+    answer = a;
+    multiply = true;
+    return String.format("%d %c %d", c, divOperator, b);
   }
 
   @Override
   public boolean check(int answer) {
-    return true;
+    return this.answer == answer;
   }
-
+  
+  private void init(int edge) {
+    a = random.nextInt(edge - 1) + 2;
+    b = random.nextInt(edge - 1) + 2;
+    c = a * b;
+  }
 }
